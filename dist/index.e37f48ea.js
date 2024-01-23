975b9b260f117a52302166c05142f145f2139acf
g.sagiannis@deddie.gr <g.sagiannis@deddie.gr>
2024-01-23 08:32:05 +0200

    NaN is fixed for ing.quantinty new Fraction

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -2588,7 +2588,7 @@ class RecipeView {
                   <svg class="recipe__icon">
                     <use href="${0, _iconsSvgDefault.default}#icon-check"></use>
                   </svg>
-                  <div class="recipe__quantity">${new (0, _fractional.Fraction)(ing.quantity).toString()}</div>
+                  <div class="recipe__quantity">${ing.quantity ? new (0, _fractional.Fraction)(ing.quantity).toString() : ""} </div>
                   <div class="recipe__description">
                     <span class="recipe__unit">${ing.unit}</span>
                     ${ing.description}
